de0c78c537b5b8435ad94033928ac40a8d93e8d7
NiiSuu143 <sunandaleitanthem888@gmail.com>
2026-09-04 23:29:21 +0530

    advanced pattern is learned. And it is done...

--- L4_Pattern/AdvancedPattern.java
+++ L4_Pattern/AdvancedPattern.java
@@ -1,6 +1,5 @@
 package Java.L4_Pattern;
 
-
 public class AdvancedPattern {
     public static void hollow_rectangle(int totRow, int totCol) {
         // outer loop for rows
@@ -50,10 +49,116 @@ public class AdvancedPattern {
             System.out.println();
         }
     }
+
+    public static void triangle_01(int n) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                if((i+j)%2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void butterflyPattern(int n) {
+        // for upper half 
+        for(int i = 1; i <= n; i++) {
+            // for left side stars
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // for space
+            for(int j = 1; j <= 2*(n-i); j++) {
+                System.out.print(" ");
+            }
+            // for right side stars
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // for lower half 
+        for(int i = n; i >= 1; i--) {
+            // for left side stars
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // for space
+            for(int j = 1; j <= 2*(n-i); j++) {
+                System.out.print(" ");
+            }
+            // for right side stars
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void solid_rhombus(int n) {
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int j=1; j<=n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void hollow_rhombus(int n) {
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int j=1; j<=n; j++) {
+                if(i==1 || i==n || j==1 || j==n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void diamond_pattern(int n) {
+        // upper half triangle
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int j=1; j<=2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // lower half triangle
+        for(int i=n; i>=1; i--) {
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int j=1; j<=2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         // hollow_rectangle(4, 5);
         // inverted_half_pyramid(4);
         // inverted_half_pyramid_with_number(5);
-        floyd_traiangle(5);
+        // floyd_traiangle(5);
+        // triangle_01(5);
+        // butterflyPattern(4);
+        // solid_rhombus(5);
+        // hollow_rhombus(5);
+        diamond_pattern(4);
     }
 }
