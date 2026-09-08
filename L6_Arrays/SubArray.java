0a8bd56c71809d3f15b3667ac884c0f326c8fd7f
NiiSuu143 <sunandaleitanthem888@gmail.com>
2026-09-08 20:48:49 +0530

    Kadane's algorithm for max sum of subarray is learned..

--- L6_Arrays/SubArray.java
+++ L6_Arrays/SubArray.java
@@ -29,7 +29,7 @@ public class SubArray {
                 for(int k=start; k<=end; k++) {
                     sumOfsub += arr[k];
                 }
-                // System.out.print(" --> Sum = "+sumOfsub );
+                System.out.println(" --> Sum = "+sumOfsub );
                 // max amoung sum of subarrays
                 if(largest<sumOfsub) {
                     largest = sumOfsub;
@@ -38,9 +38,79 @@ public class SubArray {
         }
         System.out.println(largest+ " is the largest sum.");
     }
+
+    public static void optimizedMaxSubarray(int arr[]) {
+         // print subarray -> max. sum
+        int largest = Integer.MIN_VALUE;
+        int prefix[] = new int[arr.length];
+        prefix[0] = arr[0];
+
+        for(int i=1; i<arr.length; i++) {
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+
+        for(int i=0; i<arr.length; i++) {
+            int start = i;
+            for(int j=i; j<arr.length; j++) {
+                int end = j;
+                int sumOfsub = start == 0 ? prefix[end] : prefix[end] - prefix[start-1];
+                System.out.println(" --> Sum = "+sumOfsub );
+                // max amoung sum of subarrays
+                if(largest<sumOfsub) {
+                    largest = sumOfsub;
+                }
+            }
+        }
+        System.out.println(largest+ " is the largest sum.");
+    }
+
+    public static void kadanes(int arr[]) {
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        boolean isNegative = true;
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i]>0) {
+                isNegative = false;
+                break;
+            } 
+        }
+        if(isNegative == true) {
+            for(int i=0; i<arr.length; i++) {
+                currSum += arr[i];
+                System.out.print("CurrSum = "+currSum);
+
+                maxSum = Math.max(currSum, maxSum);
+
+                System.out.print("  ----> MaxSum = "+ maxSum);
+                System.out.println();
+            }
+            System.out.println("Max sum of subarray = "+maxSum);
+        } else {
+            for(int i=0; i<arr.length; i++) {
+                currSum += arr[i];
+                if(currSum<0) {
+                    currSum = 0;
+                }
+                System.out.print("CurrSum = "+currSum);
+    
+                maxSum = Math.max(currSum, maxSum);
+    
+                System.out.print("  ----> MaxSum = "+ maxSum);
+                System.out.println();
+            }
+            System.out.println("Max sum of subarray = "+maxSum);
+        }
+    }
+
+
     public static void main(String[] args) {
-        int arr[] = {2, 4, 6, 8, 10};
-        printSubarray(arr);
-        maxSubarray(arr);
+        // int arr[] = {2, 4, 6, 8, 10};
+        // printSubarray(arr);
+        // maxSubarray(arr);
+        // int arr[] = {1, -2, 6, -1, 3};
+        // optimizedMaxSubarray(arr);
+        int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        // int arr[] = {-1, -2, -3, -4};
+        kadanes(arr);
     }
 }
