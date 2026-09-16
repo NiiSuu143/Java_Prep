bff0f1fe7c181090fe95bef4451430f64f1cd890
NiiSuu143 <sunandaleitanthem888@gmail.com>
2026-09-16 23:23:29 +0530

    New topic of string is started and all the basic concepts are learned and palindrome string problem is also solved..

---
+++ L9_Strings/PalindromeString.java
@@ -0,0 +1,41 @@
+package Java.L9_Strings;
+
+public class PalindromeString {
+    public static boolean checkPalindrome1(String str) {
+        for(int i=0; i<str.length()/2; i++) {
+            int n = str.length();
+            if(str.charAt(i) != str.charAt(n-i-1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void checkPalindrome(String str) {
+        boolean isPalindrome = false;
+        int first = 0;
+        int last = str.length()-1;
+        while(first <= last) {
+            if(str.charAt(first) == str.charAt(last)) {
+                isPalindrome = true;
+                first++;
+                last--;
+            } else {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if(isPalindrome == true) {
+            System.out.println(str+ " is a palindrome");
+        } else {
+            System.out.println(str+ " is not a palindrome");
+        }
+    }
+    public static void main(String[] args) {
+        // String str = "racecar";
+        // String str1 = "noon";
+        String str2 = "madam";
+        // checkPalindrome(str2);
+        System.out.println(checkPalindrome1(str2));
+    }
+}
---
+++ L9_Strings/Strings.java
@@ -0,0 +1,34 @@
+package Java.L9_Strings;
+// import java.util.Scanner;
+
+public class Strings {
+    public static void printLetters(String str) {
+        for(int i=0; i<str.length(); i++) {
+            System.out.print(str.charAt(i)+ " ");
+        }
+    }
+    public static void main(String[] args) {
+        // char arr[] = {'a', 'b', 'c', 'd'};
+        // String str = "abcd";
+        // String str1 = new String("xyz@#1234");
+
+        // // Strings are IMMUTABLE...(means it cant be changed...)
+
+        // Scanner sc = new Scanner(System.in);
+        // // String str2 = sc.next();    // print only single word
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // // length of the string
+        // String fullname = "Dabii the king";
+        // System.out.println(fullname.length());  // print the length with the combination of space and letters
+
+
+        // Concatenation
+        String firstname = "Sunanda";
+        String lastname = "Leitanthem";
+        String fullname = firstname+" "+lastname;
+        // System.out.println(fullname.charAt(0));
+        printLetters(fullname);
+    }
+}
