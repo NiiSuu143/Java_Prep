b4a49999420722005252d254ac81affb4eed3d1b
NiiSuu143 <sunandaleitanthem888@gmail.com>
2026-09-20 00:35:47 +0530

    Binary Operation are learned...

--- L10_Bit_Manipulation/BitManipulation.java
+++ L10_Bit_Manipulation/BitManipulation.java
@@ -2,12 +2,16 @@ package Java.L10_Bit_Manipulation;
 
 public class BitManipulation {
     public static void main(String[] args) {
-        System.out.println(5 & 6);
+        System.out.println("Binary AND = "+ (5&6));
 
-        System.out.println(5 | 6);
+        System.out.println("Binary OR = "+(5|6));
 
-        System.out.println(5 ^ 6);
+        System.out.println("Binary XOR = "+(5^6));
 
-        System.out.println(~5);
+        System.out.println("NOT 5 = "+(~5));
+
+        System.out.println("Binary left shift = "+ (5 << 2));
+
+        System.out.println("Binary righ shift = "+(6>>1));
     }
 }
