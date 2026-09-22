d8f1a71bc86c32b13dff8ca2801aae6439130cb4
NiiSuu143 <sunandaleitanthem888@gmail.com>
2026-09-23 00:42:11 +0530

    updated 2....

--- L3_Loops/Factorial.java
+++ L3_Loops/Factorial.java
@@ -2,7 +2,8 @@ import java.util.*;
 public class Factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num, fac = 1;
+        int num;
+        int fac = 1;
         System.out.print("Enter a number : ");
         num = sc.nextInt();
         for(int i = 1; i<= num; i++) {
