13d5d55a21d8a1997bdf22121c8f206c230ab86d
NiiSuu143 <sunandaleitanthem888@gmail.com>
2026-09-22 17:40:59 +0530

    Started learning the basic concept of OOPs and learned about access modifier and its scopes and also about getter & setter..

---
+++ L11_OOPs/OOPs.java
@@ -0,0 +1,50 @@
+package Java.L11_OOPs;
+
+public class OOPs {
+    public static void main(String args[]) {
+        Pen p1 = new Pen(); // created a object called p1
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+        p1.setTip(5);
+        System.out.println(p1.tip);
+
+        // if we want to change color of pen 
+        p1.color = "Yellow";
+        System.out.println(p1.color);
+
+        BankAccount myAcc = new BankAccount();
+        myAcc.userName = "Dabii";
+        myAcc.setPassword("sdfasjfd"); 
+    }
+}
+
+class BankAccount {
+    public String userName;
+    private String password;
+    public void setPassword(String pwd) {
+        password = pwd;
+    }
+}
+
+class Pen {
+    String color;
+    int tip;
+
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    void setTip(int newTip) {
+        tip = newTip;
+    }
+}
+
+class Student {
+    String name;
+    int age;
+    float percentage; 
+
+    void calcPercentage(int phy, int chem, int math) {
+        percentage = (phy + chem + math)/3;
+    }
+}
